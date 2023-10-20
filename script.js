444f4f95acee621c5448d9316867ef80ab3ff852
gauthamthampi <146702107+gauthamthampi@users.noreply.github.com>
2023-10-20 14:20:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+function javaApply1(){
+    alert("Are you sure want to leave this page to FAQ");
+}
+
+function javaApply2(){
+    alert("Are you sure want to leave this page to Investor Relations");
+}
+
+function javaApply3(){
+    alert("Are you sure want to leave this page to Privacy");
+}
+
+function javaApply4(){
+    alert("Are you sure want to leave this page to Speed Test");
+}
+
+function javaApply5(){
+    alert("Are you sure want to leave this page to Help Centre");
+
+}
+
+function javaApply6(){
+    alert("Are you sure want to leave this page to Jobs");
+
+}
+
+function javaApply7(){
+    alert("Are you sure want to leave this page to Cookie Preference");
+}
+
+function javaApply8(){
+    alert("Are you sure want to leave this page to Legal Notices");
+}
+
+function javaApply9(){
+    alert("Are you sure want to leave this page to Media Centre");
+}
+
+function javaApply10(){
+    alert("Are you sure want to leave this page Terms of use");
+}
+
+function javaApply11(){
+    alert("Are you sure want to leave this page to Contact us");
+}
